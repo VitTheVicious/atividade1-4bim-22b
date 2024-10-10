66367315ad71592e9c020c00bcef6a3c2078dbc2
VitTheVicious <142924753+VitTheVicious@users.noreply.github.com>
2024-10-10 13:49:28 -0300

    Add files via upload

---
+++ atividade1/script.js
@@ -0,0 +1,9 @@
+
+function mult() {
+    const numero1 = Number(document.querySelector("1n").value);
+    const numero2 = Number(document.querySelector("2n").value);
+    const numero3 = Number(document.querySelector("3n").value);
+
+
+}
+
