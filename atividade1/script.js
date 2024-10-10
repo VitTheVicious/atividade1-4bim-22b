0acf332754d0e0ebd9262fad768969aabc433d0b
VitTheVicious <142924753+VitTheVicious@users.noreply.github.com>
2024-10-10 14:07:20 -0300

    Update script.js

--- atividade1/script.js
+++ atividade1/script.js
@@ -1,9 +1,12 @@
-
 function mult() {
-    const numero1 = Number(document.querySelector("1n").value);
-    const numero2 = Number(document.querySelector("2n").value);
-    const numero3 = Number(document.querySelector("3n").value);
+    const numero1 = Number(document.querySelector("#n1").value);
+    const numero2 = Number(document.querySelector("#n2").value);
+    const numero3 = Number(document.querySelector("#n3").value);
 
+    const paragrafo = document.querySelector(".resultado")
 
+    paragrafo.innerHTML = `Resultado: ${numero1*numero2*numero3}`
 }
 
+const button = document.querySelector("button")
+button.addEventListener('click', mult)
